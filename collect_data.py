4d3e71c398acac06d23d7322804c3547a92368a5
Felix Giovanni Virgo <felix.giovanni.v@mail.ugm.ac.id>
2020-06-04 21:32:05 +0900

    Initial commit

---
+++ collect_data.py
@@ -0,0 +1,62 @@
+import wikipedia
+import nltk
+
+
+def merge_sentences(sents1, sents2):
+    output = []
+    if len(sents1) == len(sents2):
+        for i in range(len(sents1)):
+            sent = ""
+            sent = sents1[i] + "\t" + sents2[i]
+            output.append(sent)
+
+    return output
+
+
+def main():
+    wiki_page = wikipedia.page("World War I")
+    wiki_page2 = wikipedia.page("World War II")
+    corpus = wiki_page.content + ". " + wiki_page2.content
+
+    nltk.download('punkt')
+    corpus_splitted = nltk.tokenize.sent_tokenize(corpus.replace('\r', ' ').replace('\n', ' ')
+                                                  .replace('===== ', '').replace('==== ', '').replace('=== ', '')
+                                                  .replace('== ', '').replace('   ', ' ').replace('  ', ' '))
+    print(corpus_splitted[:10])
+    print(len(corpus_splitted))
+
+    dictionary = dict.fromkeys(['1', '!', 'q', 'Q', 'a', 'A', 'z', 'Z'], "1")
+    dictionary.update(dict.fromkeys(['2', '@', 'w', 'W', 's', 'S', 'x', 'X'], "2"))
+    dictionary.update(dict.fromkeys(['3', '#', 'e', 'E', 'd', 'D', 'c', 'C'], "3"))
+    dictionary.update(dict.fromkeys(['4', '$', 'r', 'R', 'f', 'F', 'v', 'V'], "4"))
+    dictionary.update(dict.fromkeys(['5', '%', 't', 'T', 'g', 'G', 'b', 'B'], "5"))
+    dictionary.update(dict.fromkeys(['6', '^', 'y', 'Y', 'h', 'H', 'n', 'N'], "6"))
+    dictionary.update(dict.fromkeys(['7', '&', 'u', 'U', 'j', 'J', 'm', 'M'], "7"))
+    dictionary.update(dict.fromkeys(['8', '*', 'i', 'I', 'k', 'K', ',', '<'], "8"))
+    dictionary.update(dict.fromkeys(['9', '(', 'o', 'O', 'l', 'L', '.', '>'], "9"))
+    dictionary.update(dict.fromkeys(['0', ')', 'p', 'P', ';', ':', '/', '?'], "0"))
+    dictionary.update(dict.fromkeys(['-', '_', '[', '{', '\'', '\"'], "-"))
+    dictionary.update(dict.fromkeys(['=', '+', ']', '}', '|', '\\'], "="))
+
+    print(dictionary)
+
+    corpus_converted = []
+    for sentence in corpus_splitted:
+        sentence_converted = sentence
+        for char in sentence:
+            if char in dictionary:
+                sentence_converted = sentence_converted.replace(char, dictionary[char])
+        corpus_converted.append(sentence_converted)
+
+    print(corpus_converted[:10])
+    print(len(corpus_converted))
+
+    merged = merge_sentences(corpus_converted, corpus_splitted)
+
+    with open("dataset.txt", "w") as writer:
+        for line in merged:
+            writer.write(line+"\n")
+
+
+if __name__ == '__main__':
+    main()
